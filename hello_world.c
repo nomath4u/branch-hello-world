07b2cd331644583b0adafb57f71e9ae736d227e4
Michael Harper <mharper@standalonecode.com>
2013-11-01 13:42:21 -0700

    Added boolean to function.

--- hello_world.c
+++ hello_world.c
@@ -1,9 +1,14 @@
 #include <stdio.h> //Needed for printf to work
-void add_message (){
-	printf("Hello World\n");
+void add_message (int include_newline){
+	printf("Hello World");
+	if (include_newline)
+	{
+		printf("\n");
+	}
 }
 
 int main (int argc, char **argv){
-	add_message();
+	add_message(0);
+	add_message(1);
 	return 0;
 }
